d54c5f9eb92d1b60604a8bc9fa1c9914b3347b2d
Riccy-AI <riccywidjhaya00@gmail.com>
2026-07-24 16:56:47 +0700

    setup for commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<x-layout>
+
+    <x-slot:title>
+        Welcome!
+    </x-slot:title>
+
+    <div class="max-w-2x1 mx-auto">
+        <div class="class bg-base-100 shadow mt-8">
+            <div class="card-body">
+                <div>
+                    <h1 class="text-3x1 font-bold">Welcome to Chirper!</h1>
+                    <p class="mt-4 text-base-content/60">This is your brand new Laravel application. Time to make it sing (or Chirp)!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</x-layout>
